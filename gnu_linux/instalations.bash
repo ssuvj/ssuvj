753b670afc67b74eab7421d65613037359a97c7e
ssuvj <125145157+ssuvj@users.noreply.github.com>
2026-07-24 00:59:09 +0300

    change19

--- gnu_linux/instalations.bash
+++ gnu_linux/instalations.bash
@@ -1,54 +1,55 @@
 # if from zero helpfull i always mess up
 # 1. copyq
 sudo apt update -y
-sudo apt install copyq copyq-plugins
+sudo apt install copyq copyq-plugins -y
 
 # 2. sober
 sudo apt update -y
-sudo apt install flatpak
+sudo apt install flatpak -y
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 # reboot gnu/linux
-flatpak install flathub org.vinegarhq.Sober
+flatpak install flathub org.vinegarhq.Sober -y
 
 # 3. flakon
 sudo apt update -y
-sudo apt install falkon
+sudo apt install falkon -y
 
 # 4. freetube
+flatpak update -y
 sudo apt update -y
-flatpak install io.freetubeapp.FreeTube.flatpakref
+flatpak install io.freetubeapp.FreeTube -y
 
 # 5. xterm terminal
 sudo apt update -y
-sudo apt install xterm
+sudo apt install xterm -y
 
 # 6. inxi
 sudo apt update -y
-sudo apt install inxi
+sudo apt install inxi -y
 
 # 7. fastfetch
 sudo apt update -y
-sudo apt install fastfetch
+sudo apt install fastfetch -y
 
 # 8. htop
 sudo apt update -y
-sudo apt install htop
+sudo apt install htop -y
 
 # 9. btop
 sudo apt update -y
-sudo apt install btop
+sudo apt install btop -y
 
 # 10. dconf
 sudo apt update -y
-sudo apt install dconf-cli
+sudo apt install dconf-cli -y
 
 # 11. curl
 sudo apt update -y
-sudo apt install curl
+sudo apt install curl -y
 
 # 12. wget
 sudo apt update -y
-sudo apt install wget
+sudo apt install wget -y
 
 # 13. ssh
 sudo apt update -y
@@ -56,11 +57,11 @@ sudo apt install openssh-server -y
 
 # 14. stress
 sudo apt update -y
-sudo apt install stress
+sudo apt install stress -y
 
 # 15. scrot
 sudo apt update -y
-sudo apt install scrot
+sudo apt install scrot -y
 
 # 16. python3 and some utilities
 sudo apt update -y
@@ -72,15 +73,15 @@ sudo apt install -y python3-venv
 
 # 18. ocr
 sudo apt update -y
-sudo apt install tesseract-ocr
+sudo apt install tesseract-ocr -y
 
 # 19. kamoso
 sudo apt update -y
-sudo apt install org.kde.kamoso
+sudo apt install org.kde.kamoso -y
 
 # 20. rofi
 sudo apt update -y
-sudo apt install rofi xcape
+sudo apt install rofi xcape -y
 
 # 21. openbox
 sudo apt update -y
@@ -124,8 +125,8 @@ sudo apt install pcmanfm -y
 
 # 31. ffmpeg
 sudo apt update -y
-sudo apt install ffmpeg
+sudo apt install ffmpeg -y
 
 # 32. qdirstat
 sudo apt update -y
-sudo apt install qdirstat
+sudo apt install qdirstat -y
